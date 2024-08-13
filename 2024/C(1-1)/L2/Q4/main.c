01cf63ae54e5df804f6f4ff4dfa26a749b17d4e6
Bill <pakawatsan61@gmail.com>
2024-08-13 19:51:46 +0700

    Refactor main.c to improve code readability and maintainability

--- 2024/C(1-1)/L2/Q4/main.c
+++ 2024/C(1-1)/L2/Q4/main.c
@@ -6,19 +6,37 @@ int main(void)
 	char op;
 
 	scanf("%f %f %c", &n1, &n2, &op);
-	if(op == '+'){
-		printf("%.2f", n1 + n2);
-	}else if(op == '-'){
-		printf("%.2f", n1 - n2);
-	}else if(op == '*'){
-		printf("%.2f", n1 * n2);
-	}else if(op == '/'){
-		printf("%.2f", n1 / n2);
-	}else{
-		printf("Invalid operator");
-	}
+	// if(op == '+'){
+	// 	printf("%.2f", n1 + n2);
+	// }else if(op == '-'){
+	// 	printf("%.2f", n1 - n2);
+	// }else if(op == '*'){
+	// 	printf("%.2f", n1 * n2);
+	// }else if(op == '/'){
+	// 	printf("%.2f", n1 / n2);
+	// }else{
+	// 	printf("Invalid operator");
+	// }
 
 	//! Another way
-	//? printf("%.2f", op == '+' ? n1 + n2 : op == '-' ? n1 - n2 : op == '*' ? n1 * n2 : n1 / n2);
+	switch(op){
+		case '+':
+			printf("%.2f", n1 + n2);
+			break;
+		case '-':
+			printf("%.2f", n1 - n2);
+			break;
+		case '*':
+			printf("%.2f", n1 * n2);
+			break;
+		case '/':
+			printf("%.2f", n1 / n2);
+			break;
+		default:
+			printf("Invalid operator");
+	}
+
+	//! Hard way
+	// printf("%.2f", (op == '+') ? n1 + n2 : (op == '-') ? n1 - n2 : (op == '*') ? n1 * n2 : n1 / n2);
 	return 0;
 }
