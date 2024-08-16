c83ab317a3b55469a137c2667850945698da031f
Bill <pakawatsan61@gmail.com>
2024-08-16 11:02:40 +0700

    Refactor code for improved readability and maintainability

--- 2024/C(1-1)/L2/Q3/main.c
+++ 2024/C(1-1)/L2/Q3/main.c
@@ -3,11 +3,31 @@
 int main(){
 	int n1, n2, n3;
 	scanf("%d %d %d", &n1, &n2, &n3);
-	if(n1 == n2 && n2 == n3){
-		printf("all the same");
-	}else if(n1 == n2 || n2 == n3 || n1 == n3){
-		printf("neither");
-	}else{
-		printf("all different");
-	}
+    
+    if (n1 == n2) {
+        if (n2 == n3) {
+            printf("all the same\n");
+        } else {
+            printf("neither\n");
+        }
+    } else {
+        if (n2 == n3 || n1 == n3) {
+            printf("neither\n");
+        } else {
+            printf("all different\n");
+        }
+    }
+    
+	//? and or way
+	// if(n1 == n2 && n2 == n3){
+	// 	printf("all the same");
+	// }else if(n1 == n2 || n2 == n3 || n1 == n3){
+	// 	printf("neither");
+	// }else{
+	// 	printf("all different");
+	// }
+
+	//! Another way
+	// printf("all the same"? n1 == n2 && n2 == n3 : "all different"? n1 != n2 && n2 != n3 && n1 != n3 : "neither");
+
 }
--- 2024/C(1-1)/L2/Q4/main.c
+++ 2024/C(1-1)/L2/Q4/main.c
@@ -1,42 +1,29 @@
 #include <stdio.h>
 
-int main(void)
-{
-	float n1, n2;
-	char op;
+int main(void) {
+  float n1, n2;
+  char op;
 
-	scanf("%f %f %c", &n1, &n2, &op);
-	// if(op == '+'){
-	// 	printf("%.2f", n1 + n2);
-	// }else if(op == '-'){
-	// 	printf("%.2f", n1 - n2);
-	// }else if(op == '*'){
-	// 	printf("%.2f", n1 * n2);
-	// }else if(op == '/'){
-	// 	printf("%.2f", n1 / n2);
-	// }else{
-	// 	printf("Invalid operator");
-	// }
+  scanf("%f %f %c", &n1, &n2, &op);
+  //! Another way
+  switch (op) {
+    case '+':
+      printf("%.2f", n1 + n2);
+      break;
+    case '-':
+      printf("%.2f", n1 - n2);
+      break;
+    case '*':
+      printf("%.2f", n1 * n2);
+      break;
+    case '/':
+      printf("%.2f", n1 / n2);
+      break;
+  default:
+    printf("Invalid operator");
+  }
 
-	//! Another way
-	switch(op){
-		case '+':
-			printf("%.2f", n1 + n2);
-			break;
-		case '-':
-			printf("%.2f", n1 - n2);
-			break;
-		case '*':
-			printf("%.2f", n1 * n2);
-			break;
-		case '/':
-			printf("%.2f", n1 / n2);
-			break;
-		default:
-			printf("Invalid operator");
-	}
-
-	//! Hard way
-	// printf("%.2f", (op == '+') ? n1 + n2 : (op == '-') ? n1 - n2 : (op == '*') ? n1 * n2 : n1 / n2);
-	return 0;
+  //! Hard way
+  // printf("%.2f", (op == '+') ? n1 + n2 : (op == '-') ? n1 - n2 : (op == '*') ? n1 * n2 : n1 / n2);
+  return 0;
 }
