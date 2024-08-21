a502177f98cafcb83ec32f669b13ef6465836912
Bill <pakawatsan61@gmail.com>
2024-08-21 23:33:08 +0700

    Refactor main.c for improved code readability and maintainability

--- 2024/C(1-1)/Lab/L3/Q3/main.c
+++ 2024/C(1-1)/Lab/L3/Q3/main.c
@@ -1,1 +1,18 @@
-// YOUR CODE HERE
+#include <stdio.h>
+
+int main(void) {
+  int n1, n2, n3, n4, win = 0;
+  do {
+    scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
+    if (n1 + n2 + n3 + n4 == 42) {
+      win++;
+      printf("You win!\n");
+    } else {
+      printf("You lose!\n");
+      break;
+    }
+  } while (1);
+  printf("Number of wins: %d", win);
+
+  return 0;
+}
